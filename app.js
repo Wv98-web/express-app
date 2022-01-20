55b37aef560dcc69aeb74aeb06a2ecf353f4a038
wuwei <418060258@qq.com>
2022-01-20 10:24:02 +0800

    配置常用中间件

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const morgan = require("morgan"); // 日志输出
+const cors = require("cors"); // 为客户端提供跨域资源请求
 
 const app = express();
+
+app.use(morgan("dev"));
+
+// 解析请求体
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+	res.send("hello world");
+});
+
+app.post("/", (req, res) => {
 	res.send("hello world");
 });
 
