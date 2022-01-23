87754edca1eded2262b6e1a61aa5a176a0fa20a1
wuwei <418060258@qq.com>
2022-01-23 17:51:27 +0800

    realworld 页面引入

--- router/index.js
+++ router/index.js
@@ -2,15 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-// 用户相关路由
-router.use(require("./user"));
-// 用户资料相关路由
-router.use(require("./profile"));
-// 文章相关路由
-router.use(require("./article"));
-// 文章相关路由
-router.use(require("./tag"));
-// 文章相关路由
-router.use(require("./profile"));
+router.get("/", (req, res) => {
+	res.render("index.html");
+});
 
 module.exports = router;
