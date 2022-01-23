3d218e3ac5812e8e02c23579b549cf4c5a78e437
wuwei <418060258@qq.com>
2022-01-23 17:11:43 +0800

    静态资源引入

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const cors = require("cors"); // 为客户端提供跨域资源请求
 const router = require("./router/index");
 const errorHandler = require("./middleware/errorHandler");
 require("./model");
+const template = require("art-template");
+const path = require("path");
+const util = require("util");
 
 const app = express();
 const fs = require("fs");
+
+// view engine setup
+app.engine("html", require("express-art-template"));
+app.set("view options", {
+	debug: process.env.NODE_ENV !== "production",
+});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
 
 app.use(morgan("dev"));
 
@@ -24,6 +35,23 @@ const todos = [
 	{ id: 3, title: "吃饭3" },
 ];
 
+// 托管静态资源
+// app.use(
+// 	express.static(path.join(__dirname, "./pubilc"), {
+// 		// index: ['index.html'], // 默认展示
+// 	})
+// ); // localhost:3000/css/main.css
+// 资源托管顺序问题
+// app.use(express.static(path.join(__dirname, "./node_modules"))); // 如果node_modules文件夹中也有 /css/main.css, 将不在展示，只展示 pubilc 文件夹中的 css/mian.css 文件
+// 多个静态资源
+app.use(
+	"/pubilc",
+	express.static(path.join(__dirname, "./pubilc"), {
+		// index: ['index.html'],
+	})
+); // loaclhost:3000/pubilc/css/main.css
+app.use("/node_modules", express.static(path.join(__dirname, "./node_modules"))); // 如果想要引入 node_modules 中的 /css/main.css，加上一个路径 "/node_module"
+
 // 服务端渲染
 app.get("/", (req, res) => {
 	// 1. 普通文本
@@ -31,30 +59,37 @@ app.get("/", (req, res) => {
 	// 2. HTML 格式文本
 	// res.send("<h1>Hello World</h1>");
 	// 3. 单独文件
-	// fs.readFile("./view/index.html", (err, data) => {
+	// fs.readFile("./views/index.html", (err, data) => {
 	// 	if (err) {
 	// 		return res.status(404).send("404 Not Found");
 	// 	}
 	// 	return res.end(data);
 	// });
 	// 4. 动态页面渲染 ，有数据
-	fs.readFile("./view/index.html", "utf8", (err, data) => {
-		if (err) {
-			return res.status(404).send("404 Not Found");
-		}
-
-		let str = "";
-		todos.forEach((todo) => {
-			str += `<li>${todo.title}</li>`;
-		});
-		console.log(str);
-
-		// console.log(data);
-		const ret = data.replace("&&&", str);
-
-		res.end(ret);
+	// fs.readFile("./views/index.html", "utf8", (err, data) => {
+	// 	if (err) {
+	// 		return res.status(404).send("404 Not Found");
+	// 	}
+	// 	let str = "";
+	// 	todos.forEach((todo) => {
+	// 		str += `<li>${todo.title}</li>`;
+	// 	});
+	// 	console.log(str);
+	// 	// console.log(data);
+	// 	const ret = data.replace("&&&", str);
+	// 	res.end(ret);
+	// });
+	// 5. 模板引擎
+	res.render("index", {
+		foo: "bar",
+		todos,
 	});
 });
+
+// app.get("/pubilc/css/main.css", async (req, res) => {
+// 	const data = await util.promisify(fs.readFile)("./pubilc/css/main.css");
+// 	res.end(data);
+// });
 
 // 挂载路由
 app.use("/api", router);
