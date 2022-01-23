2bd353224319fd433c888230e70f4060993f5d57
wuwei <418060258@qq.com>
2022-01-23 14:06:43 +0800

    更新文章404和403验证

--- validator/article.js
+++ validator/article.js
@@ -1,6 +1,7 @@
 const { body, param } = require("express-validator");
 const validator = require("../middleware/validator");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const { User, Article } = require("../model");
 
 exports.createArticle = validator([
 	body("article.title").notEmpty().withMessage("文章标题不能为空"),
@@ -29,7 +30,28 @@ exports.getArticle = validator([
 	validator.isValidObjectId(["params"], "articleId"),
 ]);
 
-exports.updateArticle = validator([
-	validator.isValidObjectId(["params"], "articleId"),
-	// param('articleId').isValidObjectId()
-]);
+exports.updateArticle = [
+	validator([
+		validator.isValidObjectId(["params"], "articleId"),
+		// param('articleId').isValidObjectId()
+	]),
+	async (req, res, next) => {
+		const articleId = req.params.articleId;
+		const article = await Article.findById(articleId);
+		req.article = article;
+
+		if (!article) {
+			return res.status(404).end();
+		}
+		next();
+	},
+	async (req, res, next) => {
+		if (req.user._id.toString() !== req.article.author.toString()) {
+			return res.status(403).end();
+		}
+		next();
+	},
+];
+
+// 校验文章是否存在
+// 修改的文章作者是否是当前登录用户
