5ddd9936c5b4e1beeec671bd46fb1a2730da2266
wuwei <418060258@qq.com>
2022-01-23 15:32:57 +0800

    页面渲染

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const errorHandler = require("./middleware/errorHandler");
 require("./model");
 
 const app = express();
+const fs = require("fs");
 
 app.use(morgan("dev"));
 
@@ -16,6 +17,44 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+
+const todos = [
+	{ id: 1, title: "吃饭1" },
+	{ id: 2, title: "吃饭2" },
+	{ id: 3, title: "吃饭3" },
+];
+
+// 服务端渲染
+app.get("/", (req, res) => {
+	// 1. 普通文本
+	// res.send("Hello World");
+	// 2. HTML 格式文本
+	// res.send("<h1>Hello World</h1>");
+	// 3. 单独文件
+	// fs.readFile("./view/index.html", (err, data) => {
+	// 	if (err) {
+	// 		return res.status(404).send("404 Not Found");
+	// 	}
+	// 	return res.end(data);
+	// });
+	// 4. 动态页面渲染 ，有数据
+	fs.readFile("./view/index.html", "utf8", (err, data) => {
+		if (err) {
+			return res.status(404).send("404 Not Found");
+		}
+
+		let str = "";
+		todos.forEach((todo) => {
+			str += `<li>${todo.title}</li>`;
+		});
+		console.log(str);
+
+		// console.log(data);
+		const ret = data.replace("&&&", str);
+
+		res.end(ret);
+	});
+});
 
 // 挂载路由
 app.use("/api", router);
